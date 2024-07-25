e4b35d367d408acff0a2d09d3cf0967820a2260d
Titan2op <sayan3072723@gmail.com>
2024-07-26 00:02:41 +0530

    Add files via upload

---
+++ You-Tube-Clone/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'cf225c42eemshea2844711fe6b89p1cb8a6jsn162807134e52',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
